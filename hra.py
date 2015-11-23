f5bc6f046d5b303d31f7dc7342d4b5a9ed8783c3
Petr Viktorin <encukou@gmail.com>
2015-11-23 17:58:59 +0100

    +1 token

---
+++ hra.py
@@ -0,0 +1,6 @@
+from program import hra, zeptej_se
+from random import randrange
+
+hra(randrange(10, 21),
+    zeptej_se,
+    zeptej_se)
--- program.py
+++ program.py
@@ -7,37 +7,47 @@ def odeber_token(stav):
 
 def odeber_1_token(stav):
     print('hráč odebírá 1 tokeny')
-    stav = stav - 1
-    return stav
+    return -1
 
 def odeber_2_tokeny(stav):
     print('hráč odebírá 2 tokeny')
-    stav = stav - 2
-    return stav
+    return -2
 
 def odeber_3_tokeny(stav):
     print('hráč odebírá 3 tokeny')
-    stav = stav - 3
-    return stav
+    return -3
 
 def pridej_1_token(stav):
     print('hráč přidává 1 token')
-    stav = stav + 1
-    return stav
+    return 1
+
+def zeptej_se(stav):
+    return int(input('Hraj: '))
+
+def vyhodnot(stav, tah, predchozi_tah):
+    if tah < -3 or tah > 1 or tah == 0:
+        raise ValueError('Špatný tah')
+    if tah == 1 and predchozi_tah != -3:
+        raise ValueError('Špatný tah')
+    return stav + tah
 
 def hra(stav, strategie1, strategie2):
+    predchozi_tah = 0
     while stav > 0:
         print('Stav je', stav, 'hraje hráč 1')
-        stav = strategie1(stav)
+        tah = strategie1(stav)
+        stav = vyhodnot(stav, tah,
+                        predchozi_tah)
+        predchozi_tah = tah
         if stav <= 0:
             print('vyhrál hráč 2')
             return 2
         print('Stav je', stav, 'hraje hráč 2')
-        stav = strategie2(stav)
+        tah = strategie2(stav)
+        stav = vyhodnot(stav, tah,
+                        predchozi_tah)
+        predchozi_tah = tah
         if stav <= 0:
             print('vyhrál hráč 1')
             return 1
 
-hra(randrange(10, 21),
-    odeber_1_token,
-    odeber_2_tokeny)
